4ceb52834ee877f5ee8f6f4c5932aa91be4ff5ec
sobreiraa123 <sobreiraafonso@gmail.com>
2026-05-15 17:46:24 +0100

    databse testing

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,26 @@
-from sqlalchemy import create_engine
+from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import datetime
 
-DATABASE_URL = "postgresql://postgres:janeiro2004@localhost:5432/postgres"
-
+DATABASE_URL = "postgresql://postgres:janeiro2004@localhost:5432/GymApp"
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Mapeamento da tabela Members
+class Member(Base):
+    __tablename__ = "members"
+    id = Column(Integer, primary_key=True, index=True)
+    fullname = Column(String)
+    email = Column(String, unique=True)
+    membership_type = Column(String)
+
+# Mapeamento da tabela Checkins
+class Checkin(Base):
+    __tablename__ = "checkins"
+    id = Column(Integer, primary_key=True, index=True)
+    member_id = Column(Integer, ForeignKey("members.id"))
+    checkin_date = Column(DateTime, default=datetime.datetime.utcnow)
+    location = Column(String)
