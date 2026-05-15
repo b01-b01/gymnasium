7f18b3d42b58082707beab449d0f068095f70e9b
root <root@10.65.136.38>
2026-05-15 17:42:21 +0100

    JSON key removida e db criada e configurada

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:12345@db:5432/gymnasium"
+DATABASE_URL = "postgresql://postgres:janeiro2004@localhost:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
